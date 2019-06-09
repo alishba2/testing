8e33e71378a940d40df5690d2299ab0490ac825f
Qasmi2 <nadeemqasmi40@yahoo.com>
2019-06-09 16:43:35 +0500

    determining greater number

---
+++ greaterNumber.py
@@ -0,0 +1,16 @@
+#determining greater number
+#taking input
+
+print("Enter your 1st number")
+number1 = int(input())
+print("Enter your 2nd number")
+number2 = int(input())
+
+if number1 > number2:
+    print("1st number is greater than 2nd")
+elif number2 > number1:
+    print("2nd number in greater than 1st")
+elif number1 == number1:
+    print("1st and 2nd both are equal")
+else:
+    print("you enter wrong Numbers")
